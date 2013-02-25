c8f38894793f907d99dd47c4461a638b1cdf7407
Nikolas Goebel <nikolas.goebel@googlemail.com>
2013-02-25 21:15:57 +0100

    Parse-Trees can now be generated
    Evolved from recognizer to parser.
    Parse-trees generated and rendered with NLTK.Tree

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 PYSIFAL
 =======================
 
+[REQUIRES]
+	nltk.Tree
+	nltk.Draw
+
 A python implementation of the deterministic PARSIFAL-Parser by M. Marcus.
--- marcus.py
+++ marcus.py
@@ -1,4 +1,7 @@
-import grammar
+from grammar import load_grammar
+
+from nltk.tree import *
+from nltk.draw import tree
 
 __TERMINALS__ = {
 	'Bernd': 'N',
@@ -22,26 +25,15 @@ __TERMINALS__ = {
 def print_stack(s):
 	_s_ = 'Stack: '
 	for node in s:
-		_s_ += '%s(%s) | ' % node
+		_s_ += '%s(%s) | ' % (node.root, node.pos)
 	
 	print _s_[:-2]
 
-def match_buffer(rules, active_node, position, G, buffer):
-	'''Matches the buffer against a set of rules.'''
-	#reduced = [rule for rule in rules if position < len(rule)]
-	#if reduced != []:
-	#	rules = reduced
-
-	buf_0 = buffer[0]
-	reduced = [rule for rule in rules if position < len(rule) and rule[position] == __TERMINALS__.get(buf_0, buf_0)]
-
-	if len(reduced) == 1:
-		return reduced[0]
-	else:
-		return None
+def print_buffer(b):
+	print('Buffer: %s' % " | ".join([str(x) for x in b]))
 
 def trace(s, to, G):
-	#print("Tracing %s to %s" % (s,to))
+	print("Tracing %s to %s" % (s,to))
 
 	if s == to:
 		return True
@@ -57,8 +49,8 @@ def trace(s, to, G):
 	return False
 
 def filter_rules(s, G):
-	'''Filter a set of rules for those which lead to s'''
-	print("Filtering rules")
+	'''Filter a grammar for rules which lead to s'''
+	#print("Filtering rules")
 	for root, rules in G.iteritems():
 		for rule in rules:
 			print("%s in %s" % (s, rule))
@@ -67,6 +59,15 @@ def filter_rules(s, G):
 
 	return None
 
+class Node:
+	def __init__(self, root, pos, childs=[]):
+		self.root = root
+		self.pos = pos
+		self.node = Tree(self.root, childs)
+
+	def __str__(self):
+		return self.root
+
 def marcus(w, G, k=3):
 	w = w.split(' ')
 
@@ -74,85 +75,118 @@ def marcus(w, G, k=3):
 	buf = w[:3]
 	w_i = 0
 
-	stack.append((G['__root__'],0))
-
+	stack.append( Node(G['__root__'], 0) )
+	
 	while True:
 		print('---------------------------------------')
 		print_stack(stack)
-		print('Buffer: '+' | '.join(buf))
+		print_buffer(buf)
+		
 		active = stack.pop()
-		active_node = active[0]
-		position = active[1]
-		new_pos = position
-		print('Active: %s\n' % active_node)
+		position = active.pos
+		new_pos = active.pos
 
-		if active_node in G: # Nonterminal
-			rules = G[active_node]
+		print('Active: %s\n' % active.root)
+		
+		if active.root in G: # Nonterminal
+			rules = G[active.root]
 			
 			if rules == []:
-				return False
+				print("No rules found!")
+				return None
 
-			buf_0 = __TERMINALS__.get(buf[0], buf[0])
-			buf[0] = buf_0
-			
+			# { this can probably be optimized into one or two lines
+			buf_0 = buf[0]
+			if buf_0 in __TERMINALS__:
+				buf_0 = Node(__TERMINALS__[buf_0], 0, [buf_0])
+				buf[0] = buf_0
+			# }
+
 			rule_found = False	
 			for rule in rules:
-			#	print(rule)
-			#	print(position)
-				if position < len(rule) and buf_0 == rule[position]:
-					#print("%s == %s" % (buf_0, rule[position]))
-					#print("Matching Terminal %s" % buf_0)
+
+				# check if we can possibly be inside this rule
+				# and check if the next buffer node matches the expected node from the rule
+				
+				if active.pos < len(rule) and buf_0.root == rule[active.pos]:
 					new_pos += 1
+										
 					if new_pos >= len(rule):
-						if len(buf) < 2 or trace(__TERMINALS__.get(buf[1],buf[1]), active_node, G) == False:
+						# The rule is finished now, we can either close it
+						# or keep it open to wait for new tokens.
+					
+						if len(buf) < 2 or trace(__TERMINALS__[buf[1]], active.root, G) == False:
 							# this note can be closed and put into the buffer
-							buf[0] = active_node
+							active.node.append(buf[0].node)
+							buf[0] = active
 						else:
+							active.node.append(buf[0].node)
+
 							# we can't close this node yet
 							w_i += 1
 							buf = w[w_i:w_i+3]
-							stack.append( (active_node, new_pos) )
-							stack.append( (filter_rules(__TERMINALS__.get(buf[0], buf[0]), G), 0) )
+							
+							# return the active node to the stack, since it has to stay open
+							active.pos = new_pos
+							stack.append(active)
+							
+							# add a new node to look for
+							stack.append( Node(filter_rules(__TERMINALS__[buf[0]], G), 0) )
 						
 					else:
+						# Take the node at buf[0] and place it onto the active node
+						active.node.append(buf[0].node)
+
+						# ...and update the buffer
 						w_i += 1
 						buf = w[w_i:w_i+3]
-						stack.append( (active_node, new_pos) )		
-					
+
+						# return the updated active node to the stack, 
+						active.pos = new_pos
+						stack.append(active)
+
 					rule_found = True
 					break		
 
 			if rule_found == False:
-#				print("No Matching Terminal")
+				# No unambiguous rule found, we need to generate new expected nodes
+				# return the active node to the stack
+				stack.append(active)
+				
+				# wait for the next possible rule to match
 				rule = rules[0]
-				stack.append( (active_node, position) )
-				stack.append( (rule[position], 0) )
+				print("%s (%s)" % (" ".join(rule), position))
+				stack.append( Node(rule[position], 0) )
 
-			#print("Found rule %s -> %s" % (active_node, " ".join(rule)))
+			#print("Found rule %s -> %s" % (active.root, " ".join(rule)))
 
 		if len(stack) == 0:
 			if len(buf) == 1:
-				return True
-			return False
+				return buf[0].node # this node should by now be the root of the whole parse tree
+			return None
 
 		#raw_input()
 
-	return False
+	print("No parse found!")
+	return None
 
 def main():
 	print("------------ Parser: MARCUS ------------\n")
 	grammar_path = 'grammars/marcus.gr' #raw_input('Grammar file: ')
-	G = grammar.load(grammar_path, False)
+	G = load_grammar(grammar_path, False)
 
 	#print(trace('DET', 'VP', G))
 	#print(trace('N', 'S', G))
 
 	while True:
 		w = raw_input('\nInput: ')
-		if marcus(w, G):
-			print("[TRUE]: w is in L(G)")
-		else:
+		parse_tree = marcus(w, G)
+
+		if parse_tree is None:
 			print("[FALSE]: w not in L(G)")
+		else:
+			print("[TRUE]: w is in L(G)")
+			parse_tree.draw()
 
 if __name__ == '__main__':
 	main()
